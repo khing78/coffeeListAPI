b06a9c9903378ad2577189eb7b693c51b9dab6cf
Khing78 <khing78@odds.team>
2024-05-21 22:59:29 +0700

    vercel deploy success update api list

--- api/server.ts
+++ api/server.ts
@@ -1,7 +1,28 @@
 import express, { Request, Response } from 'express';
+import * as fs from "fs";
 const app = express();
 
 app.get("/", (req: Request, res: Response) => res.send("Express on Vercel"));
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get('/api/data', (req: Request, res: Response) => {
+  fs.readFile('api/coffee_manu.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading file:', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    try {
+      const jsonData = JSON.parse(data);
+      res.json(jsonData);
+    } catch (error) {
+      console.error('Error parsing JSON:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+});
+
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Server ready on port ${port}.`));
