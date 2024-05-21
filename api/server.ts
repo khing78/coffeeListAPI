45735d180ea753ec22ad795656ed033a19814ff1
Khing78 <khing78@odds.team>
2024-05-21 23:15:07 +0700

    add __dirname

--- api/server.ts
+++ api/server.ts
@@ -1,12 +1,15 @@
 import express, { Request, Response } from 'express';
 import * as fs from "fs";
+import path from "path";
 const app = express();
 
 app.get("/", (req: Request, res: Response) => res.send("Express on Vercel"));
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.get('/api/data', (req: Request, res: Response) => {
-  fs.readFile('./public/coffee_manu.json', 'utf8', (err, data) => {
+  const filePath = path.join(__dirname, '..', 'public', 'coffee_menu.json');
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       res.status(500).send(`Error reading file: ${err}`);
       return;
