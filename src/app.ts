4090c4bd5ac38341f14bddfb673d732117872800
Khing78 <khing78@odds.team>
2024-05-21 20:58:04 +0700

    finish

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from 'express';
+import * as fs from "fs";
+
+const app = express();
+
+app.get('/api/data', (req: Request, res: Response) => {
+  fs.readFile('src/coffee_manu.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading file:', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    try {
+      const jsonData = JSON.parse(data);
+      res.json(jsonData);
+    } catch (error) {
+      console.error('Error parsing JSON:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
