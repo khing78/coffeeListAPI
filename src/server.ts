67f8ffded34dfe5080b1cd0694a7df878811ae92
Khing78 <khing78@odds.team>
2024-05-21 22:10:25 +0700

    3

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from 'express';
 import * as fs from "fs";
 
-const app = express();
+const server = express();
 
-app.get('/api/data', (req: Request, res: Response) => {
+server.get('/api/data', (req: Request, res: Response) => {
   fs.readFile('src/coffee_manu.json', 'utf8', (err, data) => {
     if (err) {
       console.error('Error reading file:', err);
@@ -22,6 +22,6 @@ app.get('/api/data', (req: Request, res: Response) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
